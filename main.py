39d4a5af625ecce24e54eb5cb5b47f6ac1683bf7
Philip Mathee <philipmathee@gmail.com>
2021-01-16 14:50:44 +0200

    'token'

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
 import discord
 from discord.ext import commands
-import os
+import config
 
-bot = commands.Bot(command_prefix = '>')
+bot = commands.Bot(command_prefix = '>') #bot prefix
+token = config.TOKEN #loads token from config file
 
 @bot.event
 async def on_ready():
     print('Carrot is ready to chop.') #will print in console if bot launches successfully
 
+extensions = ['cogs.Extras'] # list of cogs to load
 
-
-'''
-Load extensions
-'''
-extensions = ['cogs.Extras']
-
-if __name__ == '__main__':
+if __name__ == '__main__': #loading extensions
     for ext in extensions:
         bot.load_extension(ext)
 
-bot.run(os.getenv('TOKEN'))
+bot.run(token)
